62bc1e635fced411eb72927efc6f24cca846041f
adriancgarcia <adriancgarcia@gmail.com>
2023-10-20 14:55:16 -0500

    added user and registration routes

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import login_user
+
+from src import db
+from src.accounts.models import User
+
+from .forms import RegisterForm
+
 from flask_login import login_required
 
 core_bp = Blueprint("core", __name__)
@@ -8,3 +15,21 @@ core_bp = Blueprint("core", __name__)
 @login_required
 def home():
     return render_template("core/index.html")
+
+@accounts_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        flash("You are already registered.", "info")
+        return redirect(url_for("core.home"))
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        user = User(email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user)
+        flash("You registered and are now logged in. Welcome!", "success")
+
+        return redirect(url_for("core.home"))
+
+    return render_template("accounts/register.html", form=form)
